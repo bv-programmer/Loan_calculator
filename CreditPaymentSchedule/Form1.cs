0715fa83afa01b64e08511dd5659078da370c38f
Vadim <84875711+bv-programmer@users.noreply.github.com>
2021-08-11 13:00:17 +0300

    Added export to Excel.

--- CreditPaymentSchedule/Form1.cs
+++ CreditPaymentSchedule/Form1.cs
@@ -7,13 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Threading;
 
 namespace CreditPaymentSchedule
 {
     public partial class Form1 : Form
     {
         CreditInfo CI;
-        DataTable DT;
+        System.Data.DataTable DT;
         string fileNamePaymentSchedule;
 
         public Form1()
@@ -174,7 +176,8 @@ namespace CreditPaymentSchedule
             }
             this.dataGridView1.DataSource = DT;
 
-            for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++) // в цикле скрываются нулевые значения
+            // поменять цвета в ячейках таблицы
+            for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++)
             {
                 if (this.dataGridView1.Rows[i].Cells[2].Value.ToString() == "0,00" || this.dataGridView1.Rows[i].Cells[2].Value.ToString() == "0" || this.dataGridView1.Rows[i].Cells[2].Value.ToString() == "0.00")
                     this.dataGridView1.Rows[i].Cells[2].Style.ForeColor = Color.Red;
@@ -206,32 +209,203 @@ namespace CreditPaymentSchedule
 
         private void buttonCreate_Click(object sender, EventArgs e)
         {
-            
+            CreateGraph();
         }
 
         private void buttonExport_Click(object sender, EventArgs e)
         {
-           
+            if (CI.empty)
+            {
+                MessageBox.Show("Не указаны параметры кредита\nГрафик не сформирован");
+                return;
+            }
+
+            if (backgroundWorker1.IsBusy)
+                return;
+
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "Книга Excel 2003|*.xls|Книга Excel|*.xlsx" })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    fileNamePaymentSchedule = sfd.FileName;
+
+                    progressBar1.Minimum = 0;
+                    progressBar1.Value = 0;
+                    backgroundWorker1.RunWorkerAsync(fileNamePaymentSchedule);
+                }
+            }
         }
 
         private void buttonNew_Click(object sender, EventArgs e)
         {
-            
+            this.dataGridView1.DataSource = null;
+            lblFileCreate.Text = "Процесс создания файла";
+            DefaultValuesFormElements();
+            GetDataForCalculations();
+            MakeTableStructure();
+            CreateGraph();
         }
 
+        // формирование документа EXCEL
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-           
+        {            
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            excel.DisplayAlerts = false; // отключить сообщения об ощибках
+            Workbook wb = excel.Workbooks.Add(XlSheetType.xlWorksheet); // создание книги
+            Worksheet ws = (Worksheet)excel.ActiveSheet; // создание листа
+
+            const int ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET = 7;
+
+            int endRange = DT.Rows.Count + ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET; // последняя строка графика платежей
+
+            Range range = ws.get_Range("A" + ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, "F" + endRange); // получение диапазона ячеек под таблицей
+            range.Cells.Font.Size = 14; // размер шрифта в таблице
+            range.Cells.Font.Name = "Times New Roman"; // стиль шрифта в таблице
+
+            // добавление рамок к таблице
+            range.Borders.get_Item(XlBordersIndex.xlEdgeBottom).LineStyle = XlLineStyle.xlContinuous;
+            range.Borders.get_Item(XlBordersIndex.xlEdgeRight).LineStyle = XlLineStyle.xlContinuous;
+            range.Borders.get_Item(XlBordersIndex.xlInsideHorizontal).LineStyle = XlLineStyle.xlContinuous;
+            range.Borders.get_Item(XlBordersIndex.xlInsideVertical).LineStyle = XlLineStyle.xlContinuous;
+            range.Borders.get_Item(XlBordersIndex.xlEdgeTop).LineStyle = XlLineStyle.xlContinuous;
+
+            // общая информация в шапке на странице
+            Range range1 = ws.get_Range("A1", "F5");
+            range1.Cells.Font.Size = 14;
+            range1.Cells.Font.Name = "Times New Roman";
+            ws.Cells[2, 2] = "Сумма:";
+            ws.Cells[3, 2] = "Ставка:";
+            ws.Cells[4, 2] = "Первоначальный взнос:";
+            ws.Cells[2, 3] = this.labelSum.Text;
+            string rt = (CI.creditRate * 100).ToString() + "%";
+            ws.Cells[3, 3] = rt;
+            ws.Cells[4, 3] = this.labelFirstPay.Text;
+
+            // создание колонок в таблице MS Excel
+            Range range2 = ws.get_Range("A" + ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, "F" + ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET);
+            range2.Cells.Font.FontStyle = "Bold";
+            if (DT.Columns.Count > 5)
+            {
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 1] = "Платеж";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 2] = "Остаток";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 3] = "Тело кредита";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 4] = "Проценты";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 5] = "Комиссия";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 6] = "Итоговый платеж";
+            }
+            else
+            {
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 1] = "Платеж";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 2] = "Остаток";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 3] = "Тело кредита";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 4] = "Проценты";
+                ws.Cells[ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET, 5] = "Итоговый платеж";
+            }
+
+            // заполнение таблицы MS Excel данными
+            int currentRow = ROW_NUMBER_FIRST_TABLE_CELL_ON_SHEET;
+            foreach (DataRow row in DT.Rows)
+            {
+                if (!backgroundWorker1.CancellationPending)
+                {
+                    currentRow++;
+
+                    if (DT.Columns.Count > 5)
+                    {
+                        ws.Cells[currentRow, 1] = row["Платеж"].ToString();
+                        ws.Cells[currentRow, 2] = Convert.ToDouble(row["Остаток"]);
+                        ws.Cells[currentRow, 3] = Convert.ToDouble(row["Тело кредита"]);
+                        ws.Cells[currentRow, 4] = Convert.ToDouble(row["Проценты"]);
+                        ws.Cells[currentRow, 5] = Convert.ToDouble(row["Комиссия"]);
+                        ws.Cells[currentRow, 6] = Convert.ToDouble(row["Итоговый платеж"]);
+                    }
+                    else
+                    {
+                        ws.Cells[currentRow, 1] = row["Платеж"].ToString();
+                        ws.Cells[currentRow, 2] = Convert.ToDouble(row["Остаток"]);
+                        ws.Cells[currentRow, 3] = Convert.ToDouble(row["Тело кредита"]);
+                        ws.Cells[currentRow, 4] = Convert.ToDouble(row["Проценты"]);
+                        ws.Cells[currentRow, 5] = Convert.ToDouble(row["Итоговый платеж"]);
+                    }
+                    backgroundWorker1.ReportProgress(currentRow * (100 / DT.Rows.Count));
+                }
+            }
+            endRange++;
+
+            // создать объединенную ячейку в конце графика
+            Range range3 = ws.get_Range("A" + endRange, "B" + endRange);
+            range3.Merge();
+            range3.Cells.Font.Size = 14; // размер шрифта в таблице
+            range3.Cells.Font.Name = "Times New Roman"; // стиль шрифта в таблице
+            range3.Cells.Font.FontStyle = "Bold";
+            range3.Borders.get_Item(XlBordersIndex.xlEdgeBottom).LineStyle = XlLineStyle.xlContinuous;
+            range3.Borders.get_Item(XlBordersIndex.xlEdgeRight).LineStyle = XlLineStyle.xlContinuous;
+            range3.Borders.get_Item(XlBordersIndex.xlInsideHorizontal).LineStyle = XlLineStyle.xlContinuous;
+            range3.Borders.get_Item(XlBordersIndex.xlInsideVertical).LineStyle = XlLineStyle.xlContinuous;
+            range3.Borders.get_Item(XlBordersIndex.xlEdgeTop).LineStyle = XlLineStyle.xlContinuous;
+
+            // создать ячейки для вывода итоговых результатов графика
+            Range range4 = ws.get_Range("C" + endRange, "F" + endRange);
+            range4.Cells.Font.Size = 14; // размер шрифта в таблице
+            range4.Cells.Font.Name = "Times New Roman"; // стиль шрифта в таблице
+            range4.Cells.Font.FontStyle = "Bold";
+            range4.Borders.get_Item(XlBordersIndex.xlEdgeBottom).LineStyle = XlLineStyle.xlContinuous;
+            range4.Borders.get_Item(XlBordersIndex.xlEdgeRight).LineStyle = XlLineStyle.xlContinuous;
+            range4.Borders.get_Item(XlBordersIndex.xlInsideHorizontal).LineStyle = XlLineStyle.xlContinuous;
+            range4.Borders.get_Item(XlBordersIndex.xlInsideVertical).LineStyle = XlLineStyle.xlContinuous;
+            range4.Borders.get_Item(XlBordersIndex.xlEdgeTop).LineStyle = XlLineStyle.xlContinuous;
+
+            int lastRow = ++currentRow;
+            if (DT.Columns.Count > 5)
+            {
+                ws.Cells[lastRow, 1] = "ИТОГО";
+                ws.Cells[lastRow, 3] = this.labelCredit.Text;
+                ws.Cells[lastRow, 4] = this.labelPercent.Text;
+                ws.Cells[lastRow, 5] = this.labelComis.Text;
+                ws.Cells[lastRow, 6] = this.labelTotalPayments.Text;
+            }
+            else
+            {
+                ws.Cells[lastRow, 1] = "ИТОГО";
+                ws.Cells[lastRow, 3] = this.labelCredit.Text;
+                ws.Cells[lastRow, 4] = this.labelPercent.Text;
+                ws.Cells[lastRow, 5] = this.labelTotalPayments.Text; ;
+            }
+
+            excel.Columns.AutoFit();
+            ws.Cells.HorizontalAlignment = Microsoft.Office.Interop.Excel.Constants.xlCenter;
+            excel.Visible = true;
+
+            // сохранение документа MS Excel на компьютере
+            ws.SaveAs(fileNamePaymentSchedule, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
         }
 
         private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-           
+            if (e.ProgressPercentage <= 100)
+            {
+                progressBar1.Value = e.ProgressPercentage;
+                progressBar1.Update();
+                lblFileCreate.Text = String.Format("Обработка...{0}", e.ProgressPercentage);
+            }
+            else
+            {
+                progressBar1.Value = 100;
+                progressBar1.Update();
+            }
         }
 
         private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            
+            if (e.Error == null)
+            {
+                Thread.Sleep(100);
+                lblFileCreate.Text = "Файл успешно создан";
+                progressBar1.Value = 100;
+            }
+            Thread.Sleep(1000);
+            progressBar1.Value = 0;
         }
 
         // вернуть элементам формы значения по умолчанию
